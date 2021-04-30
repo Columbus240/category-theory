97d0213fa2af900152ccf4a33c35b9dc2e27c820
Stéphane P. Desarzens <st.desarzens@bluewin.ch>
2021-04-30 16:31:04 +0200

    Avoid Anomaly in Functor/Product.v
    Removing the `lazy` leads to an anomaly in coq versions 8.13.2 and
    8.12.2.
    The bug was fixed in the development version of coq, so for that
    version, this patch is not necessary.

--- Functor/Product.v
+++ Functor/Product.v
@@ -18,7 +18,7 @@ Program Instance Product
   fobj := fun x => (F x ⨂ G x)%object;
   fmap := fun _ _ f => fmap[F] f ⨂ fmap[G] f
 }.
-Next Obligation. proper; rewrites; reflexivity. Qed.
+Next Obligation. proper. lazy. rewrites. reflexivity. Qed.
 Next Obligation. normal; reflexivity. Qed.
 
 Notation "F :*: G" := (@Product _ _ _ F%functor G%functor)
