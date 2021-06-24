9cecc05dac29bc2fcfedfd535676e3fd3e83bce8
Columbus240 <8899730+Columbus240@users.noreply.github.com>
2021-06-24 18:52:19 +0200

    Explicitly write down the discrete category
    Coq v8.10 would solve the `Program Definition` completely, without
    leaving any obligations, but Coq v8.11 and upwards wouldn’t. They always
    produced obligations.
    
    Using this patch, no version produces obligations, so all versions of
    Coq should compile.

--- Instance/Discrete.v
+++ Instance/Discrete.v
@@ -7,83 +7,18 @@ Require Import Category.Theory.Category.
 Require Import Category.Structure.Discrete.
 
 (* The discrete category over a given type/setoid. *)
-Program Definition Discrete {X : Type} (S : Setoid X) : Category :=
+Definition Discrete {X : Type} (S : Setoid X) : Category :=
   {| obj := X;
      hom x y := x ≈ y;
      homset x y := full_setoid _;
 
      id := @Equivalence_Reflexive _ _ (@setoid_equiv _ S);
-     compose := λ (x y z : X) (f : y ≈ z) (g : x ≈ y), transitivity g f
+     compose := λ (x y z : X) (f : y ≈ z) (g : x ≈ y), transitivity g f;
+
+     compose_respects _ _ _ _ _ _ _ _ _ := tt;
+     id_left _ _ _ := tt;
+     id_right _ _ _ := tt;
+
+     comp_assoc _ _ _ _ _ _ _ := tt;
+     comp_assoc_sym _ _ _ _ _ _ _ := tt;
   |}.
-Next Obligation.
-  proper.
-Qed.
-Next Obligation.
-  constructor.
-Qed.
-Next Obligation.
-  constructor.
-Qed.
-Next Obligation.
-  constructor.
-Qed.
-Next Obligation.
-  constructor.
-Qed.
-
-Lemma Discrete_is_Discrete {X : Type} (S : Setoid X) :
-  is_Discrete (Discrete S).
-Proof.
-  split.
-  - red. intros. constructor.
-  - red. intros.
-    unshelve eexists.
-    + unshelve econstructor.
-      * assumption.
-      * simpl in *. symmetry. assumption.
-      * constructor.
-      * constructor.
-    + reflexivity.
-Qed.
-
-Require Import Category.Instance.Cat.
-Require Import Category.Theory.Isomorphism.
-
-(* A category is discrete iff it is equivalent to a [Discrete]
-   category over some setoid. *)
-Proposition Discrete_char (C : obj[Cat]) :
-  (is_Discrete C) ↔ { X : Type & { S : Setoid X & C ≅ (Discrete S)}}.
-Proof.
-  split.
-  - intros.
-    destruct X.
-    exists (obj[C]).
-    exists ob_setoid.
-    unshelve econstructor.
-    + unshelve econstructor.
-      * apply Datatypes.id.
-      * simpl. intros.
-        apply i0. assumption.
-      * intros. proper.
-      * intros. simpl. exact tt.
-      * intros. simpl. exact tt.
-    + unshelve econstructor.
-      * apply Datatypes.id.
-      * simpl. intros.
-        destruct f. assumption.
-      * simpl. intros. proper.
-      * simpl. reflexivity.
-      * simpl. intros.
-        subst. simpl.
-        cat.
-    + simpl.
-      eexists.
-      * intros. reflexivity.
-      * constructor.
-    + simpl. cat.
-  - intros.
-    destruct X as [X [S ?]].
-    pose proof (Discrete_is_Discrete S).
-    apply (is_Discrete_invariant (Discrete S)); try assumption.
-    symmetry; assumption.
-Defined.
